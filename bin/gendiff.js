4e9c3ec287cf3ea40e56cbdbdfae053a5d2cf4b5
Julia Malinina <malinina.artwork@gmail.com>
2021-02-09 12:28:07 +0300

    add codeclimate badge to README.md and .eslint.yml

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,7 @@
 const program = require('commander');
 
 program
-  .version('0.0.1')
+  .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>');
