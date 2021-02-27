46d1ce68cd45f7ebc5ffdc35d6f9ee678135b961
Julia Malinina <malinina.artwork@gmail.com>
2021-02-27 22:54:44 +0300

    update index.js, add json.js

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,6 +2,7 @@ import createTree from './tree.js';
 import makePlain from './plain.js';
 import makeStylish from './stylish.js';
 import buildPathAndParse from '../parsers.js';
+import makeJson from './json.js';
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const parsedPath1 = buildPathAndParse(filepath1);
@@ -10,6 +11,8 @@ const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   switch (formatName) {
     case 'plain':
       return makePlain(tree);
+    case 'json':
+      return makeJson(tree);
     default:
       return makeStylish(tree);
   }
---
+++ src/formatters/json.js
@@ -0,0 +1,39 @@
+const getValue = (value) => {
+  const keys = Object.keys(value);
+  const innerValue = keys.map((key) => {
+    if (value[key] !== null && typeof (value[key]) === 'object') {
+      return `${key}: ${getValue(value[key])}`;
+    }
+    return `${key}: ${value[key]}`;
+  });
+  return `{ ${innerValue} }`;
+};
+
+const innerJson = (tree) => {
+  const nodes = tree.nodes.length > 0 ? tree.nodes : tree;
+  const makeFlat = (acc, prop) => {
+    const isObject = (value) => (value !== null && typeof value === 'object' ? getValue(value) : value);
+    switch (prop.status) {
+      case 'unchanged key':
+        acc.push(`{ state: 'updated', ${prop.key}: ${innerJson(prop)} }`);
+        break;
+      case 'changed':
+        acc.push(`{ state: 'updated', ${prop.key}: { valueBefore: ${isObject(prop.valueBefore)}, valueAfter: ${isObject(prop.valueAfter)}} }`);
+        break;
+      case 'deleted':
+        acc.push(`{ state: 'deleted', ${prop.key}: ${isObject(prop.valueBefore)} }`);
+        break;
+      case 'added':
+        acc.push(`{ state: 'added', ${prop.key}: ${isObject(prop.valueBefore)} }`);
+        break;
+      default:
+        return acc;
+    }
+    return acc;
+  };
+  const arr = nodes.reduce(makeFlat, []);
+  return arr;
+};
+
+const makeJson = (tree) => JSON.stringify(innerJson(tree));
+export default makeJson;
