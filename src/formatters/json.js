e14aa67115565290ff1f596f53cbeb76492d04e6
Julia Malinina <malinina.artwork@gmail.com>
2021-02-28 16:04:09 +0300

    update json.js, add typeChecker.js

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,49 +1,33 @@
-/* eslint-disable quotes */
-/* eslint-disable no-useless-escape */
+import checkType from '../typeChecker.js';
+
 const getValue = (value) => {
   const keys = Object.keys(value);
-  const innerValue = keys.map((key) => {
-    if (value[key] !== null && typeof (value[key]) === 'object') {
-      return `"${key}":${getValue(value[key])}`;
-    }
-    return `"${key}":${(value[key])}`;
-  });
-  return `{${innerValue}}`;
+  const innerValue = keys.map((key) => `"${key}":${checkType(value[key], getValue)}`);
+  return innerValue;
 };
 
-const isBoolean = (value) => {
-  if (value === '') {
-    return `\""`;
-  }
-  return !value || value === true || typeof value === 'number' ? value : `"${value}"`;
-};
-const isObject = (value) => (value !== null && typeof value === 'object' ? getValue(value) : value);
-const checkType = (value) => isBoolean(isObject(value));
-
-const innerJson = (tree) => {
+const makeJson = (tree) => {
   const nodes = tree.nodes.length > 0 ? tree.nodes : tree;
   const makeFlat = (acc, prop) => {
     switch (prop.status) {
       case 'unchanged key':
-        acc.push(`{"state":"updated","${prop.key}":${innerJson(prop)}}`);
+        acc.push(`{"state":"updated","${prop.key}":${makeJson(prop)}}`);
         break;
       case 'changed':
-        acc.push(`{"state":"updated","${prop.key}":{"valueBefore":${checkType(prop.valueBefore)},"valueAfter":${checkType(prop.valueAfter)}}}`);
+        acc.push(`{"state":"updated","${prop.key}":{"valueBefore":${checkType(prop.valueBefore, getValue)},"valueAfter":${checkType(prop.valueAfter, getValue)}}}`);
         break;
       case 'deleted':
-        acc.push(`{"state":"deleted","${prop.key}":${checkType(prop.valueBefore)}}`);
+        acc.push(`{"state":"deleted","${prop.key}":${checkType(prop.valueBefore, getValue)}}`);
         break;
       case 'added':
-        acc.push(`{"state":"added","${prop.key}":${checkType(prop.valueBefore)}}`);
+        acc.push(`{"state":"added","${prop.key}":${checkType(prop.valueBefore, getValue)}}`);
         break;
       default:
         return acc;
     }
     return acc;
   };
-  const arr = nodes.reduce(makeFlat, []);
-  return arr;
+  const json = nodes.reduce(makeFlat, []).join(',');
+  return json;
 };
-
-const makeJson = (tree) => innerJson(tree).join('');
 export default makeJson;
