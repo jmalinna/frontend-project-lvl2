eb1756ad1588e72e44028fee9d84b5e07b42d089
Julia Malinina <malinina.artwork@gmail.com>
2021-03-02 00:04:32 +0300

    update typeCheckerStylish.js

--- src/typeCheckerStylish.js
+++ src/typeCheckerStylish.js
@@ -2,12 +2,14 @@ import makeGap from './gap.js';
 
 const isObject = (value, gaps, depthLevel, func) => {
   const signAndGap = 2;
+
   if (Array.isArray(value)) {
     return `[${value.join(', ')}]`;
   }
   if (!value || typeof value !== 'object') {
     return value;
   }
+
   const innerValue = func(value, depthLevel).join('');
   return `{${innerValue}\n${makeGap(gaps + signAndGap)}}`;
 };
