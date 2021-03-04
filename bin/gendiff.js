121a53ead11731c6ed9e0a8201128ef752d1270d
Julia Malinina <malinina.artwork@gmail.com>
2021-03-05 01:15:22 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,9 +6,9 @@ commander
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format', 'stylish')
-  .arguments('<filepath1> <filepath2> [formatName]')
-  .action((filepath1, filepath2, formatName) => {
-    const diff = genDiff(filepath1, filepath2, formatName);
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    const diff = genDiff(filepath1, filepath2, options.format);
     console.log(diff);
   })
   .parse(process.argv);
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -4,7 +4,7 @@ import makeStylish from './stylish.js';
 import buildPathAndParse from '../parsers.js';
 import makeJson from './json.js';
 
-const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
+const genDiff = (filepath1, filepath2, formatName) => {
   const parsedPath1 = buildPathAndParse(filepath1);
   const parsedPath2 = buildPathAndParse(filepath2);
   const tree = createTree(parsedPath1, parsedPath2);
