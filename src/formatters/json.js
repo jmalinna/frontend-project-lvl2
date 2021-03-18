b8b49bfe3b2dce565eb8adff66e2a01450bf3caa
Julia Malinina <malinina.artwork@gmail.com>
2021-03-18 18:51:21 +0300

    update json.js and file4_after_json.txt

--- src/formatters/json.js
+++ src/formatters/json.js
@@ -1,27 +1,2 @@
-const innerJson = (tree) => {
-  const nodes = tree.nodes ? tree.nodes : tree;
-  const makeFlat = (prop) => {
-    switch (prop.status) {
-      case 'unchanged key':
-        return { key: prop.key, state: 'updated', value: innerJson(prop) };
-      case 'changed':
-        return {
-          key: prop.key, state: 'updated', valueBefore: prop.valueBefore, valueAfter: prop.valueAfter,
-        };
-      case 'deleted':
-        return { key: prop.key, state: prop.status, value: prop.valueBefore };
-      case 'added':
-        return { key: prop.key, state: prop.status, value: prop.valueBefore };
-      default:
-        return null;
-    }
-  };
-  const json = nodes.map((prop) => makeFlat(prop));
-  const filteredJson = json.filter((element) => element !== null);
-  return filteredJson;
-};
-const makeJson = (tree) => {
-  const result = innerJson(tree);
-  return JSON.stringify(result);
-};
+const makeJson = (tree) => JSON.stringify(tree);
 export default makeJson;
